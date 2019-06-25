bc610f3cb5ad272550d511a9ad33d5f66a34edd9
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-06-25 13:28:17 +0200

    Add output and log-axis options

--- tdqd_tools/compare.py
+++ tdqd_tools/compare.py
@@ -12,12 +12,23 @@ def compare_diff(data_1, data_2):
     print(f"max(|diff|) = {np.max(diff)}")
 
 
-def compare_plot(data_1, data_2, label_1, label_2):
+def compare_plot(data_1, data_2, label_1, label_2, log):
     time_points_1, vals_1 = data_1[:, 0], data_1[:, 1]
     time_points_2, vals_2 = data_2[:, 0], data_2[:, 1]
 
+    plt.figure()
     plt.plot(time_points_1, vals_1, label=label_1)
     plt.plot(time_points_2, vals_2, label=label_2)
 
+    plt.title("Data")
     plt.legend(loc="best")
+
+    plt.figure()
+
+    if log:
+        plt.semilogy(time_points_1, np.abs(vals_1 - vals_2))
+    else:
+        plt.plot(time_points_1, np.abs(vals_1 - vals_2))
+
+    plt.title("Absolute difference")
     plt.show()
