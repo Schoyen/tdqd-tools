285ee101fc967833e074c808d19f1d2282659f14
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-06-25 10:35:06 +0200

    Use time instead of timestep for laser cut-off

--- tdqd_tools/spectral_lines.py
+++ tdqd_tools/spectral_lines.py
@@ -3,19 +3,18 @@ import scipy.fftpack
 import matplotlib.pyplot as plt
 
 
-def get_spectral_lines(time_points, dipole_moment, slice_=None):
-    if slice_ is None:
-        slice_ = slice(None, None)
+def get_spectral_lines(time_points, dipole_moment, stop_laser=0):
+    mask = time_points >= stop_laser
 
     dt = time_points[1] - time_points[0]
 
     freq = (
-        scipy.fftpack.fftshift(scipy.fftpack.fftfreq(len(time_points[slice_])))
+        scipy.fftpack.fftshift(scipy.fftpack.fftfreq(len(time_points[mask])))
         * 2
         * np.pi
         / dt
     )
-    a = np.abs(scipy.fftpack.fftshift(scipy.fftpack.fft(dipole_moment[slice_])))
+    a = np.abs(scipy.fftpack.fftshift(scipy.fftpack.fft(dipole_moment[mask])))
 
     a /= a.max()
 
